0c77b9cd6a9ea26070e8dc07123d4c597613d9e4
Travis Barnes <Travis Barnes>
2016-08-16 14:52:31 -0700

    Added page_not_found.html, login.html, admin.html. Implemented basic login (no auth checks) to gain access to admin.html.

--- pairApp.py
+++ pairApp.py
@@ -29,6 +29,36 @@ def index():
 	return flask.render_template('index.html')
 
 
+@app.route("/login")
+def login():
+	app.logger.debug("Login page entry")
+
+	return flask.render_template('login.html')
+
+
+@app.route("/admin")
+@app.route("/admin", methods=['POST'])
+def admin():
+	app.logger.debug("Admin page entry")
+	## INSERT LOGIN LOGIC ##
+	## If request isn't POST and session cookie is expired, send to login page
+	## Else, send to admin page
+	#########
+	if request.method != 'POST':
+		return flask.render_template('login.html')
+	else:
+		
+		return flask.render_template('admin.html') ## Only return if login is successful
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+	app.logger.debug("Page not found")
+	flask.session['linkback'] = flask.url_for("index")
+
+	return flask.render_template("page_not_found.html")
+
+
 if __name__ == "__main__":
 	import uuid
 	app.secret_key = str(uuid.uuid4())

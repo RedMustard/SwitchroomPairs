547d116e25f1e81705a5bdb028493e735b639e5b
Travis Barnes <Travis Barnes>
2016-09-06 11:50:18 -0700

    Implemented table row selection

--- static/js/tableSelect.js
+++ static/js/tableSelect.js
@@ -1,9 +1,25 @@
-$("#table tr").click(function(){
-   $(this).addClass('selected').siblings().removeClass('selected');    
-   var value=$(this).find('td:first').html();
-   alert(value);    
+$('#databaseTable').on('click', 'tbody tr', function(event) {
+	$(this).addClass('table-info').siblings().removeClass('table-info');
+	console.log( $(this).text() );
 });
 
-$('.ok').on('click', function(e){
-    alert($("#table tr.selected td:first").html());
-});
+// $(document).on('click', 'tr', function() {
+// 	$(this).addClass('table-info');
+// });
+
+// $('#databaseView').click(function() {
+   // $(this).addClass('table-info');
+// });
+
+
+
+
+
+// window.onload = function() {
+// 	var databaseTable = document.getElementById('databaseTable');
+// }
+
+// databaseTable.onclick = function() {
+// 	// $(this).addClass('table-info');
+// 	console.log( "sadsadsadsasad" );
+// }

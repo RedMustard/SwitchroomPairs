fae646c5de957e58a73bb40b37bf10c75ffa227f
Travis Barnes <tbarnes@barnesmediadesign.com>
2016-10-17 14:46:15 -0700

    Added BASEconfig.py

---
+++ BASEconfig.py
@@ -0,0 +1,17 @@
+"""
+Duplicate this file and name 'config.py'.
+Then, in the new file, fill in the following information.
+"""
+
+## Local development environment
+sql_config_dictionary = {
+	'user': 'INSERT DATABASE USERNAME',
+	'password': 'INSERT DATABASE PASSWORD',
+	'host': '127.0.0.1',
+	'database': 'INSERT DATABASE NAME',
+	'raise_on_warnings': True
+}
+
+PORT=5000
+DEBUG=False
+

0ea4aa3e0780efce49a44def52914b05cb33c2f2
Travis Barnes <tbarnes@barnesmediadesign.com>
2016-10-31 13:09:38 -0700

    Added more instructions to data_insert.py

--- data_insert.py
+++ data_insert.py
@@ -4,10 +4,17 @@ This program functions as a method to insert an initial batch of entries
 from a CSV file into the database for the University of Oregon/Century Link 
 Demarcation & Cross-Connect app.
 
-The CSV file should contain a header row in the following format:
+
+Make sure to run pairApp.py at least one time before running this script.
+
+
+The CSV file should contain a header row with the following format:
 	Circuit ID, Type, CL Pair, UO Pair, Customer, Customer Phone, Notes
-	
-	The data for the CSV file should be appear in the same format as the header.
+
+The data for the CSV file should be appear in the same format as the header.
+For example, the file should look similar to:
+	Circuit ID, Type, CL Pair, UO Pair, Customer, Customer Phone, Notes
+	541-555-5555, Tel #, 1501, 302, John Doe, 541-555-5555, This is a test
 """
 
 import db, fileinput
